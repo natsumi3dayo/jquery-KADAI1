4bbc0e58807655621d39b37ef4d3b867620af600
natsumi3dayo <95215163+natsumi3dayo@users.noreply.github.com>
2022-03-19 17:01:40 +0900

    first commit

--- main.js
+++ main.js
@@ -1,33 +1,52 @@
-// タイマー部
-let stop;
-let progress;
-let addition = 0;
-const record = document.querySelector("p.counter");
-// カウンター
-function timer() {
-const start = new Date().getTime();
-stop = setInterval(function() {
-progress = new Date().getTime() - start + addition;
-const noms = progress / 1000;
-const millisecond = progress ? ("0" + String(noms).split(".")[1]).slice(-2) : "00";
-const nos = Math.trunc(noms);
-const second = nos ? ("0" + (nos % 86400 % 3600 %60)).slice(-2) : "00";
-const minute = nos >= 60 ? ("0" + Math.trunc(nos % 86400 % 3600 / 60)).slice(-2) : "00";
-const hour = nos >= 360 ? ("0" + Math.trunc(nos % 86400 / 3600)).slice(-2) : "00";
-if (progress < 86400) {
-record.textContent = hour + ":" + minute + "." + second + "." + millisecond; } else {
-record.textContent = "00:00.00.00"; clearInterval(stop); }}, 10); }
-// ボタン部
-const startButton = document.querySelector("button.start");
-const stopButton = document.querySelector("button.stop");
-const resetButton = document.querySelector("button.reset");
+//ボタン部//
+const Getdisplay =document.querySelector("display");
+const startButton =document.querySelector("button.start");
+const stopButton =document.querySelector("button.stop");
+const resetButton =document.querySelector("button.reset");
 stopButton.disabled = true; resetButton.disabled = true;
-// スタート
-startButton.addEventListener("click", function() {
-progress = 0; timer(); startButton.disabled = true; stopButton.disabled = false; resetButton.disabled = false; });
-// ストップ
-stopButton.addEventListener("click", function() {
-clearInterval(stop); addition = progress; startButton.disabled = false; stopButton.disabled = true; resetButton.disabled = false; });
-// リセット
-resetButton.addEventListener("click", function() {
-clearInterval(stop); progress = 0; record.textContent = "00:00.00.00"; addition = 0; startButton.disabled = false; stopButton.disabled = true; resetButton.disabled = true; });
+
+//タイマー部//
+let hours = 0;
+let minutes= 0;
+let seconds= 0;
+let seconds2= 0;
+
+let Getinterval;
+
+function stopwatch(){
+  seconds2++;
+   if(seconds2 / 10 == 1 ){
+     seconds++;
+     seconds2= 0;
+     if( seconds / 10 == 1){  
+       minutes++;
+       seconds = 0;
+       if( minutes / 10 == 1 ){
+        hours++
+        minutes = 0 ;
+     }
+    }
+   }
+   display.innerHTML=hours+":"+minutes+":"+seconds+":"+seconds2 ;
+}
+
+//スタートボタン//
+startButton.addEventListener("click",function(){
+  startButton.disabled = true; stopButton.disabled = false; resetButton.disabled = false;
+    Getinterval = setInterval(stopwatch,100);
+    Getstatus = "start";
+})
+
+//ストップボタン//
+stopButton.addEventListener("click",function(){
+  startButton.disabled = false; stopButton.disabled = true; resetButton.disabled = false;
+  clearInterval(Getinterval);
+})
+
+
+//リセットボタン//
+resetButton.addEventListener("click",function(){
+startButton.disabled = false; stopButton.disabled = true; resetButton.disabled = true;
+clearInterval(Getinterval);
+display.innerHTML = "0:0:0:0";
+})
